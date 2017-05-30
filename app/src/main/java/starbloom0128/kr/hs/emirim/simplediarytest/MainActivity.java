8d8eaf981f64581b7c9d659c99a29c05bbf52713
seungah <fofo2525@naver.com>
2017-05-30 20:49:17 +0900

    #3 Current date and DatePicker Init and Handler

--- app/src/main/java/starbloom0128/kr/hs/emirim/simplediarytest/MainActivity.java
+++ app/src/main/java/starbloom0128/kr/hs/emirim/simplediarytest/MainActivity.java
@@ -6,10 +6,13 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
     DatePicker date;
     EditText edit;
     Button but;
+    String fileName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +21,25 @@ public class MainActivity extends AppCompatActivity {
         date = (DatePicker)findViewById(R.id.date_pick);
         edit = (EditText)findViewById(R.id.edit);
         but = (Button)findViewById(R.id.but);
+
+        Calendar cal = Calendar.getInstance();
+        int year=cal.get(Calendar.YEAR);
+        int month=cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DATE);
+
+        date.init(year, month, day, new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker datePicker, int year, int month, int day) {
+                fileName=year+"_"+(month+1)+"_"+day+".txt";
+                String readData = readDiary(fileName);
+                edit.setText(readData);
+                but.setEnabled(true); //수정이나 새로 작성
+
+            }
+        });
+
+    }
+    public String readDiary(String fileName){
+        return null;
     }
 }
