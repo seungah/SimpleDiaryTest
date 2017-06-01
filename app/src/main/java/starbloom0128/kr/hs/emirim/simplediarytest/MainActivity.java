6452314b83bd2dac5c1c5a8aaba60c15bbcd00c3
seungah <fofo2525@naver.com>
2017-06-01 19:02:38 +0900

    #4 readDiary() completion

--- app/src/main/java/starbloom0128/kr/hs/emirim/simplediarytest/MainActivity.java
+++ app/src/main/java/starbloom0128/kr/hs/emirim/simplediarytest/MainActivity.java
@@ -6,6 +6,9 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,20 +21,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        date = (DatePicker)findViewById(R.id.date_pick);
-        edit = (EditText)findViewById(R.id.edit);
-        but = (Button)findViewById(R.id.but);
+        date = (DatePicker) findViewById(R.id.date_pick);
+        edit = (EditText) findViewById(R.id.edit);
+        but = (Button) findViewById(R.id.but);
 
         Calendar cal = Calendar.getInstance();
-        int year=cal.get(Calendar.YEAR);
-        int month=cal.get(Calendar.MONTH);
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
         int day = cal.get(Calendar.DATE);
 
         date.init(year, month, day, new DatePicker.OnDateChangedListener() {
             @Override
             public void onDateChanged(DatePicker datePicker, int year, int month, int day) {
-                fileName=year+"_"+(month+1)+"_"+day+".txt";
-                String readData = readDiary(fileName);
+                fileName = year + "_" + (month + 1) + "_" + day + ".txt";
+                String readData = null;
+                try {
+                    readData = readDiary(fileName);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
                 edit.setText(readData);
                 but.setEnabled(true); //수정이나 새로 작성
 
@@ -39,7 +47,22 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
-    public String readDiary(String fileName){
-        return null;
+    public String readDiary(String fileName) throws FileNotFoundException {
+        String diaryStr = null;
+        FileInputStream fIn = openFileInput(fileName);
+        try {
+            fIn = openFileInput(fileName);
+            byte[] buf = new byte[500];
+            fIn.read(buf);
+            diaryStr = new String(buf).trim();
+            but.setText("수정 하기");
+        } catch (FileNotFoundException e) {
+            edit.setText("일기가 존재하지 않습니다.");
+            but.setText("새로 저장");
+        } catch (IOException e) {
+
+        }
+
+        return diaryStr;
     }
 }
